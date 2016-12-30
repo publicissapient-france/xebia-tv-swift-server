c0cef6ae648b1369132db0d1283fb6dadf1aa94f
Simone Civetta <viteinfinite@gmail.com>
2016-12-30 12:08:46 +0100

    First Commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,69 @@
+import Vapor
+import Foundation
+import Core
+import HTTP
+
+let drop = Droplet()
+let dataLoader = DataFile()
+
+drop.get("/categories") { req in
+    let fileBody = try dataLoader.load(path: drop.workDir + "Data/categories.json")
+    return Response(body: .data(fileBody))
+}
+
+drop.get("/playlistItems") { req in
+    let fileBody = try dataLoader.load(path: drop.workDir + "Data/youtube.json")
+    return Response(body: .data(fileBody))
+}
+
+drop.get("/search") { req in
+    let fileBody = try dataLoader.load(path: drop.workDir + "Data/youtube_search.json")
+    return Response(body: .data(fileBody))
+}
+
+private func launchLiveStreamer(videoId: String) -> (success: Bool, data: Data?) {
+    return shell("/usr/local/bin/livestreamer", args: ["https://www.youtube.com/watch?v=" + videoId, "--json", "--stream-url"])
+}
+
+drop.get("/video", String.self) { req, videoId in
+    let liveStreamerResult = launchLiveStreamer(videoId: videoId)
+    if !liveStreamerResult.success {
+        return try Response(status: .internalServerError, json: JSON(node: [:]))
+    }
+    
+    guard let data = liveStreamerResult.data else {
+        return try Response(status: .noContent, json: JSON(node: [:]))
+    }
+    
+    guard let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [AnyHashable : Any],
+        let streams = jsonObject["streams"] as? [AnyHashable : Any]
+    else {
+        return try Response(status: .noContent, json: JSON(node: [:]))
+    }
+    
+    let urls = streams
+        .flatMap { key, value -> Node? in
+            guard let stringKey = key as? String,
+                let dictValue = value as? [AnyHashable : Any],
+                let urlString = dictValue["url"] as? String
+            else { return nil }
+            
+            return Node.object([
+                "type": "video/mp4",
+                "quality": Node.string(stringKey),
+                "url": Node.string(urlString)
+            ])
+    }
+    
+    return try Response(status: .ok, json: JSON(node: Node.array(urls)))
+}
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
