775b6cbaffc4777654b336370d625280e31c500c
Simone Civetta <viteinfinite@gmail.com>
2017-04-02 14:39:55 +0200

    [vapor] Update vapor version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "XebiaTV",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
     ],
     exclude: [
         "Config",
