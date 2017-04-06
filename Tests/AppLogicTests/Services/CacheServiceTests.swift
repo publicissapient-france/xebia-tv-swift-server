cef3d1d29a630acb9fde346625b1363f5a486261
Simone Civetta <viteinfinite@gmail.com>
2017-04-06 22:43:29 +0200

    [cache] Add tests

---
+++ Tests/AppLogicTests/Services/CacheServiceTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import Vapor
+import HTTP
+@testable import AppLogic
+
+class CacheServiceTests: XCTestCase {
+    
+    func testExpiration() throws {
+        let drop = try makeTestDroplet()
+        let key = "A"
+        
+        let redisService = RedisService(drop: drop)
+        let node = Node(stringLiteral: "a-value")
+        try redisService.save(node: node, with: key, expiration: "30")
+        
+        let ttl = try redisService.ttl(for: key)
+        XCTAssertEqual(ttl, 30)
+    }
+}
